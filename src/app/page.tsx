8fc66611a9c97f066009327b1d90dd45479ece61
Heroic Matt <florinvenis@gmail.com>
2024-05-15 21:33:14 +0300

    Adaugat sistem de logare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { RedirectToSignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>test</h1>
-    </main>
+    <>
+    <SignedIn>
+      <main className="">
+        <UserButton/>
+      </main>
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+  </>
   );
 }
