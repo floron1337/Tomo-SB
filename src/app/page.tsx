06b357d3a278bdebf9d072d8355747e8ca36bddb
PD GP <petru.draghiceanu@gmail.com>
2024-05-16 00:38:01 +0300

    Implementare postari de tip text + postari tip foto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,56 @@
-import { RedirectToSignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 import TextPost from "../components/TextPost";
+import ImagePost from "@/components/ImagePost";
 
 export default function Home() {
   return (
     <>
-    <SignedIn>
-        <main className="flex h-screen w-screen">
-          <div className="flex flex-row justify-center p-24 h-screen w-screen bg-background">
-            <TextPost></TextPost>
-            <UserButton/>
+      <SignedIn>
+        <main className="h-auto overflow-x-auto">
+          <div className="position absolute w-screen h-10 bg-[#08171b] text-white font-bold p-2">
+            Tomo
+          </div>
+
+          <div className="h-screen p-10 overflow-scroll overflow-x-hidden">
+            <div className="flex flex-col gap-8 justify-center items-center pt-5 h-auto w-screen bg-background">
+              <TextPost
+                user={"nigger"}
+                likes={0}
+                friends={0}
+                content={
+                  "Cel mai mare nas pe care l am vazut in viata mea a fost atunci cand l- am intalnit pe Florin Venis"
+                }
+                comments={0}
+              ></TextPost>
+              <ImagePost
+                user={"nigger"}
+                likes={0}
+                friends={0}
+                photo={"/yosemite.png"}
+                description="Om cu nas mare mere in parc"
+                title="Intalnirea mea cu spinul"
+                content={
+                  "Cel mai mare nas pe care l am vazut in viata mea a fost atunci cand l- am intalnit pe Florin Venis"
+                }
+                comments={0}
+              ></ImagePost>
+
+              {/*<div className="h-auto">
+              <UserButton />
+              </div>*/}
+            </div>
           </div>
         </main>
-    </SignedIn>
-    <SignedOut>
-      <RedirectToSignIn />
-    </SignedOut>
-  </>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
   );
 }
